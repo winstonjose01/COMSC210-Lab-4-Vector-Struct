c278faf5e0d32f49812b131658c2be6a906e1626
winstonjose01 <winston.jose@gmail.com>
2024-09-06 16:39:19 -0700

    revision-1

---
+++ vector_structs.cpp
@@ -0,0 +1,21 @@
+// COMSC-210 | Lab 4 | Winston Jose
+//
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+
+int main(){
+
+    vector<int> myVec(Color color_1);
+
+}
