a3cda14a1f28983ebaa3474dff59e5c26d2efaa3
winstonjose01 <winston.jose@gmail.com>
2024-09-06 23:36:01 -0700

    revision-2

--- vector_structs.cpp
+++ vector_structs.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <random>
 using namespace std;
 
 
@@ -13,9 +14,26 @@ struct Color {
     int blue;
 };
 
-
 int main(){
 
-    vector<int> myVec(Color color_1);
+    Color temp;
 
+    random_device seed;
+    mt19937 gen{seed()};
+    uniform_int_distribution<> n_distr{25,50};
+    int n = n_distr(gen);
+    cout << "n =" << n;
+
+    vector<Color> myVec(n);
+
+    uniform_int_distribution<> color_distr{0,255};
+    for (int i = 0; i < n; i++){
+        temp.red = color_distr(gen);
+        temp.green = color_distr(gen);
+        temp.blue = color_distr(gen);
+        myVec.push_back(temp);
+    }
+    for (const Color& val : myVec){
+        cout << val.red << "\t" << val.green << "\t" << val.blue << endl;
+    }
 }
