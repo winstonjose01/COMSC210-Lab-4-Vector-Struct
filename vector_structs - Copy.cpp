579559142ebc5e197314f042219abff4ab78504f
winstonjose01 <winston.jose@gmail.com>
2024-09-07 08:18:42 -0700

    Final revision

--- vector_structs - Copy.cpp
+++ vector_structs - Copy.cpp
@@ -15,6 +15,7 @@ struct Color {
 };
 
 int main(){
+
     Color temp;
     // Seed the random generator
     random_device seed;
@@ -23,26 +24,23 @@ int main(){
     uniform_int_distribution<> n_distr{25,50};
     int n = n_distr(gen);
 
-    //Create an empty vector of type struct
     vector<Color> myVec(n);
-    myVec.clear(); //Initialize the vector
-    
+    myVec.clear();
     // Generate random integers between 0-255 RGB color
     uniform_int_distribution<> color_distr{0,255};
     for (int i = 0; i < n; i++){
         temp.red = color_distr(gen);
         temp.green = color_distr(gen);
         temp.blue = color_distr(gen);
-        //Populate the vector with the temp struct items
         myVec.push_back(temp);
     }
-    // Print the header for the table
-    cout << setw(4) << "Color" << setw(8) << "Red" << setw(11) << "Green" << setw(8) << "Blue" << endl;
+
+    cout << setw(4) << "Color" << setw(8) << "Red" << setw(9) << "Green" << setw(8) << "Blue" << endl;
+    cout << setw(4) << "-----" << setw(8) << "---" << setw(9) << "-----" << setw(8) << "----" << endl;
     int i =  1;
-    // Loop and print the vector contents in a table
     for (const Color& val : myVec){
-        cout << setw(4) << "-----" << setw(9) << "-----" << setw(10) << "-----" << setw(9) << "-----" << endl;
-        cout << setw(3) << i << setw(10) << val.red << setw(9) << val.green << setw(9) << val.blue << endl;
+        cout << setw(3) << i << setw(10) << val.red << setw(8) << val.green << setw(8) << val.blue << endl;
+        cout << setw(4) << "-----" << setw(8) << "---" << setw(9) << "-----" << setw(8) << "----" << endl;
         i++;
     }
 }
