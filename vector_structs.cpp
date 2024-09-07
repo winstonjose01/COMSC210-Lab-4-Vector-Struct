db5aeb43757cd32c3f1f9ee19ddaf6164531402d
winstonjose01 <winston.jose@gmail.com>
2024-09-06 23:53:35 -0700

    revision2

--- vector_structs.cpp
+++ vector_structs.cpp
@@ -22,9 +22,9 @@ int main(){
     mt19937 gen{seed()};
     uniform_int_distribution<> n_distr{25,50};
     int n = n_distr(gen);
-    cout << "n =" << n;
 
     vector<Color> myVec(n);
+    myVec.clear();
 
     uniform_int_distribution<> color_distr{0,255};
     for (int i = 0; i < n; i++){
@@ -33,7 +33,11 @@ int main(){
         temp.blue = color_distr(gen);
         myVec.push_back(temp);
     }
+
+    cout << setw(4) << "Color" << setw(8) << "Red" << setw(8) << "Green" << setw(8) << "Blue" << endl;
+    int i =  1;
     for (const Color& val : myVec){
-        cout << val.red << "\t" << val.green << "\t" << val.blue << endl;
+        cout << setw(4) << i << setw(8) << val.red << setw(8) << val.green << setw(8) << val.blue << endl;
+        i++;
     }
 }
